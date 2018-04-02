39769b71f8d15c0d0b025b2eb66e42dd9a611fcb
Vsevolod Lomovitsky <vsevolod.lomovitskiy@corp.codeinside.ru>
2018-04-02 13:23:43 +0300

    basic item addition and deletion

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 import './Item.css';
 import logo from './folder.svg';
 
+import { guid } from '../../helpers';
+
 export default class Item extends Component {
     state = {
-        showItems: false
+        showItems: false,
+        items: this.props.items
     }
 
     handleClick = (e) => {
@@ -12,8 +15,29 @@ export default class Item extends Component {
         this.setState({ showItems: !this.state.showItems });
     }
 
+    handleAddItem = (e, name = 'new item') => {
+        e.preventDefault();
+        e.stopPropagation();
+        this.setState({
+            items: [...this.state.items, {
+                name,
+                guid: guid(),
+                items: []
+            }],
+            showItems: true
+        });
+    }
+
+    handleDeleteItem = (e, guid) => {
+        e.preventDefault();
+        e.stopPropagation();
+        this.setState({
+            items: this.state.items.filter(item => item.guid !== guid)
+        });
+    }
+
     render() {
-        const { name, items } = this.props;
+        const { name, guid } = this.props;
 
         return <div className="Item">
             <div className="Item__element" onClick={this.handleClick}>
@@ -23,9 +47,27 @@ export default class Item extends Component {
                 <div className="Item__text">
                     { name }
                 </div>
+                <button
+                    className="Item__button"
+                    onClick={(e) => this.handleAddItem(e)}
+                >
+                    add
+                </button>
+                <button
+                    className="Item__button"
+                    onClick={(e) => this.props.handleDeleteItem(e, guid)}
+                >
+                    del
+                </button>
             </div>
             { this.state.showItems ? <div className="Item__list">
-                { items.map((item, index) => <Item name={item.name} items={item.items} key={index} />) }
+                { this.state.items.map((item, index) => <Item
+                    name={item.name}
+                    items={item.items}
+                    guid={item.guid}
+                    key={index}
+                    handleDeleteItem={this.handleDeleteItem}
+                />) }
             </div> : null }
         </div>
     }
